e18e543a3ffbe4928484689995f5b6bab6431852
ksilas <silaslelei@ymail.com>
2026-04-14 15:00:31 +0300

    feat: add error boundary for no word in dictionary or same language conversion

--- chichwa/translate/services/translator.py
+++ chichwa/translate/services/translator.py
@@ -3,7 +3,7 @@ def translate_text(text, source, target):
     EN_TO_SW = {
         "hello" : "jambo",
         "tea" : "chai",
-        "gari" : "car", 
+        "car" : "gari", 
         "fish" : "samaki",
         "thanks" : "asante",
         "please" : "tafadhali",
@@ -20,8 +20,29 @@ def translate_text(text, source, target):
     for k,v in EN_TO_SW.items():
         SW_TO_EN[f"{v}"]=k
 
+    translated_words = []
+
+    to_translate = text.split(" ")
+
     if source.lower() == "en" and target.lower()== "sw":
-        return EN_TO_SW[text.lower()]
+        for i in to_translate:
+            if i in EN_TO_SW:
+                translated_words.append(EN_TO_SW[i.lower()])
+            else:
+                return "No such word in dictionary"
+        return " ".join(translated_words)
     
     if source.lower() == "sw" and target.lower()== "en":
-        return SW_TO_EN[text.lower()]
+        for i in to_translate:
+            if i in SW_TO_EN:
+                translated_words.append(SW_TO_EN[i.lower()])
+            else:
+                return "No such word in dictionary"
+
+        return " ".join(translated_words)
+    
+    if source.lower() == "sw" and target.lower()== "sw":
+        return text
+    
+    if source.lower() == "en" and target.lower()== "en":
+        return text
