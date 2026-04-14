c493516db7699b9cd540179275d399a8499f815c
ksilas <silaslelei@ymail.com>
2026-04-14 15:32:09 +0300

    feat: implement rest api

--- chichwa/translate/views.py
+++ chichwa/translate/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
 from .services import translator
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-
+@csrf_exempt
 def translate(request):
-    translated = None
-    if request.method == 'POST':
-        source_lang = request.POST.get('source_lang')
-        target_lang = request.POST.get('target_lang')
-        text = request.POST.get('text')
+    if request.method != "POST":
+        return JsonResponse(
+            {"error" : "only POST method allowed"},
+            status= 403
+        )
+    
+    try:
+        body = json.loads(request.body)
+        text = body.get("text")
+        target_lang = body.get("target_lang")
+        source_lang = body.get("source_lang")
+        translated = None
+        
+        if not text or not target_lang or not source_lang:
+            return JsonResponse(
+                {"error" : "missing required fields"}
+            )
+        
+
 
         translated = translator.translate_text(text, source_lang, target_lang)
-    return render(request,"index.html", {"translated": translated})
+        return JsonResponse({"translated":translated})
     
+    except json.JSONDecodeError:
+        return JsonResponse(
+            {"error" : "Invalid JSON"},
+            status = 400
+        )
