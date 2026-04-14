b9f206d43491cbeccff233bc2a54040a6c92de58
ksilas <silaslelei@ymail.com>
2026-04-14 13:45:47 +0300

    feat: add app to settings; add url to app; add translator view to handle request

--- chichwa/translate/views.py
+++ chichwa/translate/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .services import translator
 
 # Create your views here.
+
+
+def translate(request):
+    translator.translate_text("Hello ","en","sw")
+    return render(request,"index.html")
