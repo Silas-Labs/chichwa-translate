afeddb809946e27cf4fcdd0796005b7750b9a8bb
ksilas <silaslelei@ymail.com>
2026-04-14 15:01:03 +0300

    feat: get input from user

--- chichwa/translate/views.py
+++ chichwa/translate/views.py
@@ -5,5 +5,12 @@ from .services import translator
 
 
 def translate(request):
-    translated = translator.translate_text("Hello","en","sw")
+    translated = None
+    if request.method == 'POST':
+        source_lang = request.POST.get('source_lang')
+        target_lang = request.POST.get('target_lang')
+        text = request.POST.get('text')
+
+        translated = translator.translate_text(text, source_lang, target_lang)
     return render(request,"index.html", {"translated": translated})
+    
